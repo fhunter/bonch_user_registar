4c34d5d918365a1c60f50a5ac2e3ffef9ca5258a
Nikolay Zamotaev <fhunter@fhunter.mine.nu>
2014-01-11 03:47:30 +0400

    Indent + fix show/hide

--- admin/ui.js
+++ admin/ui.js
@@ -1,11 +1,13 @@
 function div_toggle(div_id){
-  	var t=document.getElementById(div_id);
-	for(i=0;i<t.parentElement.getElementsByTagName("div").length;i++){
-	  p=t.parentElement.getElementsByTagName("div")[i];
-	  if(p.id==div_id){
-	    p.style.display ="inline";
-	  }else{
-	    p.style.display ="none";
-	  }
-	}
+  var t=document.getElementById(div_id);
+  for(i=0;i<t.parentElement.getElementsByTagName("div").length;i++){
+    p=t.parentElement.getElementsByTagName("div")[i];
+    if(p.parentElement.id == t.parentElement.id ){
+      if(p.id==div_id){
+	p.style.display ="inline";
+      }else{
+	p.style.display ="none";
+      }
+    }
+  }
 }
