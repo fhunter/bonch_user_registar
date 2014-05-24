58351f7a06ba57283160db608f3ade60d3b88f24
Nikolay Zamotaev <fhunter@fhunter.mine.nu>
2014-05-24 15:43:15 +0400

    Added queue

--- database.sql
+++ database.sql
@@ -1,4 +1,5 @@
 PRAGMA foreign_keys=OFF;
 BEGIN TRANSACTION;
 CREATE TABLE users (id auto primary key, username text unique, fio text,studnum text, photo blob);
+CREATE TABLE queue (id auto primary key, username text not null, password text not null, date datetime not null default current_timestamp, done boolean not null default 'false');
 COMMIT;
