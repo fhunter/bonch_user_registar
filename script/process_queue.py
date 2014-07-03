072af492fe02a1e34b4a7e4ec3017341cf519cd3
Nikolay Zamotaev <fhunter@fhunter.mine.nu>
2014-07-03 14:13:28 +0400

    Fixes

--- script/process_queue.py
+++ script/process_queue.py
@@ -14,7 +14,7 @@ if "REMOTE_ADDR" in os.environ:
 conn = sqlite3.connect("/var/www/selfreg/database.sqlite3")
 cursor = conn.cursor()
 #cursor.execute('insert into queue (username, password) values (?, ?)', t)
-cursor.execute('select * from queue where done=\'false\'')
+cursor.execute('select * from queue where done=\'false\' order by date asc')
 queue = cursor.fetchall()
 conn.commit()
 conn.close()
@@ -22,17 +22,16 @@ if queue != None:
 	conn = sqlite3.connect("/var/www/selfreg/database.sqlite3")
 	cursor = conn.cursor()
 	for i in queue:
-		print i
 		commandline=u"""kadmin -p automator/admin -k -t /etc/krb5.keytab -q "change_password -pw %s %s" """ % (i[2], i[1], )
-		print commandline
 		try:
 			retcode = subprocess.call(commandline, shell=True)
 			if retcode < 0:
 				print >>sys.stderr, "Child was terminated by signal", -retcode
 			else:
-				print >>sys.stderr, "Child returned", retcode
 				if retcode == 0:
-					cursor.execute('update queue set done= ? where username = ? and password = ?',(True,i[1],i[2]))
+					cursor.execute('update queue set done= ? where username = ? and password = ?',('true',i[1],i[2]))
+				else:
+					print >>sys.stderr, "Child returned", retcode
 		except OSError as e:
 			print >>sys.stderr, "Execution failed:", e
 	conn.commit()
