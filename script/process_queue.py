e5ec7a55905f153bf768510523aefbd84db4b8c1
fhunter <fhunter@fhunter.link>
2023-06-24 03:55:00 +0200

    Commandline tools

--- script/process_queue.py
+++ script/process_queue.py
@@ -1,38 +1,39 @@
 #!/usr/bin/env python3
 import os
-import sqlite3
 import subprocess
 import sys
 
 if "REMOTE_ADDR" in os.environ:
-	print "Content/type: text/html"
-	print ""
-	print "Wrong page"
-	exit(0)
+    print("Content/type: text/html")
+    print("")
+    print("Wrong page")
+    exit(0)
 
 sys.path.insert(1, os.path.join(sys.path[0], '..'))
-from my_db import db_exec_sql
+from my_db import Session, Queue
 
 #This is not a CGI
-queue = db_exec_sql('select * from queue where done=False order by date asc')
-if queue != None:
-	for i in queue:
-		commandline=u"""kadmin -p automator/admin -k -t /etc/krb5.keytab -q "change_password -pw %s %s" """ % (i[2], i[1], )
-		try:
-			retcode = subprocess.call(commandline, shell=True)
-			if retcode < 0:
-				print >>sys.stderr, "Child was terminated by signal", -retcode
-			else:
-				if retcode == 0:
-				    	db_exec_sql('update queue set done=True where username = %s and password = %s', (i[1],i[2]))
-				else:
-					print >>sys.stderr, "Child returned", retcode
-		except OSError as e:
-			print >>sys.stderr, "Execution failed:", e
+s = Session()
+queue = s.query(Queue).filter(Queue.done == 0).order_by(Queue.date.asc()).all()
+for i in queue:
+    username = i.username.username
+    password = i.password
+    commandline=f"""kadmin -p automator1/admin -k -t /etc/krb5.keytab -q "change_password -pw {password} {username}" """
+    try:
+        retcode = subprocess.call(commandline, shell=True)
+        if retcode < 0:
+            print(f"Child was terminated by signal {-retcode}", file=sys.stderr)
+        else:
+            if retcode == 0:
+                i.done = True
+            else:
+                print(f"Child returned {retcode}", file=sys.stderr)
+    except OSError as e:
+        print(f"Execution failed: {e}", file=sys.stderr)
 
-		commandline=u"""kadmin -p automator/admin -k -t /etc/krb5.keytab -q "modprinc +needchange %s" """ % (i[1], )
-		try:
-			retcode = subprocess.call(commandline, shell=True)
-		except OSError as e:
-			print >>sys.stderr, "Execution failed:", e
-
+    commandline=f"""kadmin -p automator1/admin -k -t /etc/krb5.keytab -q "modprinc +needchange {username}" """ 
+    try:
+        retcode = subprocess.call(commandline, shell=True)
+    except OSError as e:
+        print(f"Execution failed: {e}", file=sys.stderr)
+s.commit()
--- script/update_quota_base.py
+++ script/update_quota_base.py
@@ -1,30 +1,46 @@
 #!/usr/bin/env python3
 
-import requests
-from requests_kerberos import HTTPKerberosAuth
-import sys
+import time
 import subprocess
+import requests
+from requests_kerberos import HTTPKerberosAuth, DISABLED, OPTIONAL, REQUIRED
 
 top=True
 data=[]
+
+def send_data(session, url, data):
+    response = session.post(url, auth=HTTPKerberosAuth(mutual_authentication=REQUIRED), json = data)
+    return response.status_code
+
+def send_data_with_retry(session, url, data, retries):
+    counter = 0
+    code = 401
+    while (code == 401) and (counter < retries):
+        if counter > 0:
+            print(counter)
+        code = send_data(session, url, data)
+        counter = counter + 1
+        time.sleep(0.1)
+    if counter >= retries:
+        return False
+    return True
+
+s= requests.Session()
 for i in subprocess.check_output(['/usr/sbin/repquota', '-O', 'csv', '-c', '/home']).splitlines():
     if top:
-       top=False
-       continue
-    line=i.split(',')
+        top=False
+        continue
+    t=i.decode('utf-8')
+    line=t.split(',')
     if line[0]=='root':
-       continue
+        continue
     username=line[0]
     used=line[3]
     quota=line[4]
     data.append({ 'username': username, 'used': used, 'quota': quota})
-    if(len(data)>1000):
-        response = requests.post('http://eniac.dcti.sut.ru/selfreg/process/quota',auth=HTTPKerberosAuth(), json = data)
-        if response.status_code != 200:
-            print(response.json())
+    if len(data)>1000:
+        send_data_with_retry(s, 'http://srv-1.dcti.sut.ru/selfreg/process/quota', data, 50)
         data=[]
 
 
-response = requests.post('http://eniac.dcti.sut.ru/selfreg/process/quota',auth=HTTPKerberosAuth(), json = data)
-if response.status_code != 200:
-    print(response.json())
+send_data_with_retry(s, 'http://srv-1.dcti.sut.ru/selfreg/process/quota', data, 50)
