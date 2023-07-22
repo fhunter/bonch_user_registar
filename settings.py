de6dc70e44c68352e85f98b8fc5f30d081d3352c
fhunter <fhunter@fhunter.link>
2023-07-22 16:28:46 +0200

    Renamed personal group

--- settings.py
+++ settings.py
@@ -2,5 +2,5 @@
 
 PREFIX='/selfreg'
 
-ADMINGROUPS=['personal','teachers']
+ADMINGROUPS=['passreset','teachers']
 QUOTAPROCESS=['fhunter','eshur','op','automator/admin','automator1/admin','automator2/admin']
