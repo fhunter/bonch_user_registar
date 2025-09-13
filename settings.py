294e3d1ceff8ba8b7e44d4a72fd766c673705f29
fhunter <fhunter@fhunter.link>
2025-09-13 14:14:36 +0200

    Updated minimal size

--- settings.py
+++ settings.py
@@ -2,7 +2,7 @@
 
 PREFIX='/selfreg'
 
-QUOTA_UNUSED=170
+QUOTA_UNUSED=173
 
 ADMINGROUPS=['passreset','teachers']
 QUOTAPROCESS=['fhunter','eshur','op','automator/admin','automator1/admin','automator2/admin']
